7b0477589505b8aba1b29f05dd8306a151366411
Fabiana Fernandes Ramos <fframos051182@gmail.com>
2024-05-28 15:50:33 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-novo-loja-ebac/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('fabiana.teste@teste.com.br')
+        cy.get('#password').type('Juli160710')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, fabiana.teste (não é fabiana.teste? Sair)')
+    });
+})
