c3b33c5c001dcb3b81d27f42b21328c26d65482e
Fabiana Fernandes Ramos <fframos051182@gmail.com>
2024-06-03 18:00:48 -0300

    Usando massa de dados e fixture

--- cypress/e2e/3-novo-loja-ebac/login.cy.js
+++ cypress/e2e/3-novo-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -21,7 +22,7 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('fabianafr@teste.com.br')
         cy.get('#password').type('Juli160710')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
         cy.get('.woocommerce-error').should('exist')
 
     });
@@ -30,7 +31,27 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('fabiana.teste@teste.com.br')
         cy.get('#password').type('Juli')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail fabiana.teste@teste.com.br está incorreta. Perdeu a senha?')
-        
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail fabiana.teste@teste.com.br está incorreta. Perdeu a senha?')
+
     });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, fabiana.teste (não é fabiana.teste? Sair)')
+
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario , { log: false })
+            cy.get('#password').type(dados.senha , { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, fabiana.teste (não é fabiana.teste? Sair)')
+    
+        })
+
+    });
+
 })
